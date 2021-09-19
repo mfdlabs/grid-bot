d4de989bb214ab05ba71980c130cd8cac8ace4dd
Nikita Petko <petko@mfdlabs.com>
2021-09-19 22:45:16 +0100

    Update MFDLabs.Discord.RoleSets

--- Assemblies/MFDLabs.Discord.RoleSets/Properties/AssemblyInfo.cs
+++ Assemblies/MFDLabs.Discord.RoleSets/Properties/AssemblyInfo.cs
@@ -1,36 +1,24 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+#if !DEBUG
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: CompilationRelaxations(8)]
+#endif
 [assembly: AssemblyTitle("MFDLabs.Discord.RoleSets")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Role Set helpers for discord.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("MFDLABS")]
 [assembly: AssemblyProduct("MFDLabs.Discord.RoleSets")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyCopyright("Copyright © MFDLABS 2019. All rights reserved.")]
+[assembly: AssemblyTrademark("MFDLABS Who Owns This Sector? (R)")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("af62128f-7566-41ec-bf74-278050fa1651")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(false)]
+[assembly: AssemblyVersion("1.0.*")]
