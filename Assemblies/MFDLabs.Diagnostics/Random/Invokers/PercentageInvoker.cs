743874a043750d9a68fa5c33cbc372281c38f0e4
Nikita Petko <petko@vmminfra.net>
2022-01-28 20:34:24 +0000

    Add Random, TODO: MFDLabs.Random??

---
+++ Assemblies/MFDLabs.Diagnostics/Random/Invokers/PercentageInvoker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MFDLabs.Diagnostics
+{
+	public static class PercentageInvoker
+	{
+		public static void InvokeAction(Action action, int invokePercentage)
+		{
+			if (invokePercentage < 0 || invokePercentage > 100)
+				throw new ArgumentOutOfRangeException("invokePercentage must be between 0 and 100.");
+			if (_rng.Next() % 100 < invokePercentage) action();
+		}
+
+		private static readonly IRandom _rng = RandomFactory.GetDefaultRandom();
+	}
+}
---
+++ Assemblies/MFDLabs.Diagnostics/Random/Random/RngCryptoServiceRandom.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace MFDLabs.Diagnostics
+{
+	public class RngCryptoServiceRandom : IRandom
+	{
+        public RngCryptoServiceRandom() => _longRng = new LongRandom(new Action<byte[]>(NextBytes));
+
+        public int Next()
+		{
+			var data = new byte[4];
+			_rng.GetBytes(data);
+			return BitConverter.ToInt32(data, 0) & int.MaxValue;
+		}
+        public int Next(int maxValue) => Next(0, maxValue);
+        public int Next(int minValue, int maxValue)
+		{
+			if (minValue > maxValue) throw new ArgumentOutOfRangeException();
+			return (int)((long)Math.Floor(minValue + (maxValue - minValue) * NextDouble()));
+		}
+        public void NextBytes(byte[] buffer) => _rng.GetBytes(buffer);
+        public double NextDouble()
+		{
+			var data = new byte[4];
+			_rng.GetBytes(data);
+			return BitConverter.ToUInt32(data, 0) / float.MaxValue;
+		}
+        public long NextLong() => NextLong(long.MaxValue);
+        public long NextLong(long maxValue)
+		{
+			if (maxValue < 0) throw new ArgumentOutOfRangeException(nameof(maxValue));
+			return NextLong(0, maxValue);
+		}
+        public long NextLong(long minValue, long maxValue) => _longRng.NextLong(minValue, maxValue);
+
+        private readonly RandomNumberGenerator _rng = new RNGCryptoServiceProvider();
+		private readonly LongRandom _longRng;
+	}
+}
