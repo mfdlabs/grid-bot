a39dc9cb49698b51640575c7d99916c6cd4bcb06
Nikita Petko <petko@vmminfra.net>
2022-01-29 00:54:41 +0000

    Update Percent Invoker to return a boolean to determine if we hit the action

--- Assemblies/MFDLabs.Diagnostics/Random/Invokers/PercentageInvoker.cs
+++ Assemblies/MFDLabs.Diagnostics/Random/Invokers/PercentageInvoker.cs
@@ -4,11 +4,17 @@ namespace MFDLabs.Diagnostics
 {
 	public static class PercentageInvoker
 	{
-		public static void InvokeAction(Action action, int invokePercentage)
+		public static bool InvokeAction(Action action, int invokePercentage)
 		{
 			if (invokePercentage < 0 || invokePercentage > 100)
-				throw new ArgumentOutOfRangeException("invokePercentage must be between 0 and 100.");
-			if (_rng.Next() % 100 < invokePercentage) action();
+				throw new ArgumentOutOfRangeException(nameof(invokePercentage), "invokePercentage must be between 0 and 100.");
+			if (_rng.Next() % 100 < invokePercentage)
+            {
+				action();
+				return true;
+			}
+
+			return false;
 		}
 
 		private static readonly IRandom _rng = RandomFactory.GetDefaultRandom();
