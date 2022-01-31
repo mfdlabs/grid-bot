92c3cdbb995354c61d716c1f08cc820983f3002e
Nikita Petko <petko@vmminfra.net>
2022-01-31 00:07:11 +0000

    Add other DispatcherQueues

--- Shared/MFDLabs.GridTasks/WorkQueues/WorkQueueDispatcherQueueRegistry.cs
+++ Shared/MFDLabs.GridTasks/WorkQueues/WorkQueueDispatcherQueueRegistry.cs
@@ -7,5 +7,7 @@ namespace MFDLabs.Grid.Bot.Tasks.WorkQueues
     internal static class WorkQueueDispatcherQueueRegistry
     {
         public static readonly DispatcherQueue RenderQueue = new PatchedDispatcherQueue("Render Queue", new(0, "Render Queue Dispatcher"));
+        public static readonly DispatcherQueue ScriptExecutionQueue = new PatchedDispatcherQueue("Script Execution Queue", new(0, "Script Execution Queue Dispatcher"));
+        public static readonly DispatcherQueue GridServerScreenshotQueue = new PatchedDispatcherQueue("Grid Server Screenshot Queue", new(0, "Grid Server Screenshot Queue Dispatcher"));
     }
 }
