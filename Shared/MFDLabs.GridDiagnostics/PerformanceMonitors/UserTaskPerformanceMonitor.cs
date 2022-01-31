31970919a5a653636ddb7fedd9df47c8562becc0
Nikita Petko <petko@vmminfra.net>
2022-01-31 00:08:46 +0000

    Remove task thread perf monitor

--- Shared/MFDLabs.GridDiagnostics/PerformanceMonitors/UserTaskPerformanceMonitor.cs
+++ Shared/MFDLabs.GridDiagnostics/PerformanceMonitors/UserTaskPerformanceMonitor.cs
@@ -31,26 +31,4 @@ namespace MFDLabs.Grid.Bot.PerformanceMonitors
             TotalItemsProcessedThatSucceededPerSecond = counterRegistry.GetRateOfCountsPerSecondCounter(category, "TotalItemsProcessedThatSucceededPerSecond", instance);
         }
     }
-
-    public sealed class UserTaskPerformanceMonitor
-    {
-        private const string Category = "MFDLabs.Grid.Tasks.{0}User";
-
-        public IRawValueCounter TotalItemsProcessed { get; }
-        public IRawValueCounter TotalItemsProcessedThatFailed { get; }
-        public IRawValueCounter TotalItemsProcessedThatSucceeded { get; }
-
-        public UserTaskPerformanceMonitor(ICounterRegistry counterRegistry, string kind, IUser user)
-        {
-            if (counterRegistry == null) throw new ArgumentNullException(nameof(counterRegistry));
-
-            var instance = $"{user.Username}@{user.Id}";
-
-            var category = string.Format(Category, kind);
-
-            TotalItemsProcessed = counterRegistry.GetRawValueCounter(category, "TotalItemsProcessed", instance);
-            TotalItemsProcessedThatFailed = counterRegistry.GetRawValueCounter(category, "TotalItemsProcessedThatFailed", instance);
-            TotalItemsProcessedThatSucceeded = counterRegistry.GetRawValueCounter(category, "TotalItemsProcessedThatSucceeded", instance);
-        }
-    }
 }
