9d4cd469a40e1e98cbe8ef563f8f8280bba2cfcf
Nikita Petko <petko@vmminfra.net>
2022-02-06 17:14:24 +0000

    Ops/better build configurations (#10)
    * Major change to build configurations, should be self explanitory
    
    * Update README

--- BuildConfigIntegration.ps1
+++ BuildConfigIntegration.ps1
@@ -1,1 +1,1 @@
-.\Build.ps1 -solutionName MFDLabs.Grid -buildKind MFDLabs.Grid.Bot -restoreSolution $true -cleanObjAndBinFolders $false -buildConfig "Release"
+.\Build.ps1 -solutionName MFDLabs.Grid -buildKind MFDLabs.Grid.Bot -restoreSolution $true -cleanObjAndBinFolders $false -buildConfig "ReleaseDeploy"
