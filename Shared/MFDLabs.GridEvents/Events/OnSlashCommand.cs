fd8aa002c3b0d13dc8d55f18f478ef19b6e3217d
Nikita Petko <petko@vmminfra.net>
2022-02-24 20:14:42 +0000

    GRIDBOT-17 and #28 (Fix discord gateway hang): Use a concurrent work queue to fix?

--- Shared/MFDLabs.GridEvents/Events/OnSlashCommand.cs
+++ Shared/MFDLabs.GridEvents/Events/OnSlashCommand.cs
@@ -2,62 +2,16 @@
 
 using System.Threading.Tasks;
 using Discord.WebSocket;
-using MFDLabs.Grid.Bot.Extensions;
-using MFDLabs.Grid.Bot.Registries;
-using MFDLabs.Text.Extensions;
-using MFDLabs.Logging;
+using MFDLabs.Grid.Bot.WorkQueues;
 
 namespace MFDLabs.Grid.Bot.Events
 {
     public static class OnSlashCommand
     {
-        public static async Task Invoke(SocketSlashCommand command)
+        public static Task Invoke(SocketSlashCommand command)
         {
-            await command.User.FireEventAsync(typeof(OnMessage).FullName, command.Channel.Name);
-
-            var userIsAdmin = command.User.IsAdmin();
-            var userIsPrivilaged = command.User.IsPrivilaged();
-            var userIsBlacklisted = command.User.IsBlacklisted();
-
-            if (command.User.IsBot && !global::MFDLabs.Grid.Bot.Properties.Settings.Default.AllowParsingForBots) return;
-
-            if (!global::MFDLabs.Grid.Bot.Properties.Settings.Default.AllowAllChannels)
-            {
-                if (!command.Channel.IsWhitelisted() && !userIsAdmin)
-                    return;
-            }
-
-            if (!global::MFDLabs.Grid.Bot.Properties.Settings.Default.IsEnabled)
-            {
-                if (!userIsAdmin && !userIsPrivilaged)
-                {
-                    SystemLogger.Singleton.Warning("Maintenance enabled, and someone tried to use it!!");
-
-                    var failureMessage = global::MFDLabs.Grid.Bot.Properties.Settings.Default.ReasonForDying;
-
-                    if (!failureMessage.IsNullOrEmpty()) await command.RespondEphemeralPingAsync(failureMessage);
-
-                    return;
-                }
-            }
-
-            if (userIsBlacklisted)
-            {
-                SystemLogger.Singleton.Warning("A blacklisted user {0}('{1}#{2}') tried to use the bot, attempt to DM that they are blacklisted.", command.User.Id, command.User.Username, command.User.Discriminator);
-
-                try
-                {
-                    await command.User.SendDirectMessageAsync($"you are unable to use this bot as you've been blacklisted, to have your case reviewed, please contact <@{(global::MFDLabs.Grid.Bot.Properties.Settings.Default.BotOwnerID)}>");
-                }
-                catch
-                {
-                    SystemLogger.Singleton.Warning("We tried to DM the user, but their DMs may not be available.");
-                }
-
-                return;
-            }
-
-            await CommandRegistry.CheckAndRunSlashCommand(command);
+            OnSlashCommandReceivedWorkQueue.Singleton.EnqueueWorkItem(command);
+            return Task.CompletedTask;
         }
     }
 }
---
+++ Shared/MFDLabs.GridEvents/WorkQueues/OnSlashCommandReceivedWorkQueue.cs
@@ -0,0 +1,75 @@
+﻿#if WE_LOVE_EM_SLASH_COMMANDS
+
+using Microsoft.Ccr.Core;
+using Discord.WebSocket;
+using MFDLabs.Logging;
+using MFDLabs.Concurrency;
+using MFDLabs.Text.Extensions;
+using MFDLabs.Grid.Bot.Utility;
+using MFDLabs.Grid.Bot.Registries;
+using MFDLabs.Grid.Bot.Extensions;
+
+namespace MFDLabs.Grid.Bot.WorkQueues
+{
+    internal sealed class OnSlashCommandReceivedWorkQueue : AsyncWorkQueue<SocketSlashCommand>
+    {
+        private static readonly DispatcherQueue _DispatcherQueue = new PatchedDispatcherQueue("On Slash Command Received Work Queue", new(0, "On Slash Command Received Work Queue Dispatcher"));
+
+        public static readonly OnSlashCommandReceivedWorkQueue Singleton = new();
+
+        public OnSlashCommandReceivedWorkQueue()
+            : base(_DispatcherQueue, OnReceive)
+        { }
+
+        private static async void OnReceive(SocketSlashCommand command, SuccessFailurePort result)
+        {
+            await command.User.FireEventAsync(typeof(OnSlashCommandReceivedWorkQueue).FullName, command.Channel.Name);
+
+            var userIsAdmin = command.User.IsAdmin();
+            var userIsPrivilaged = command.User.IsPrivilaged();
+            var userIsBlacklisted = command.User.IsBlacklisted();
+
+            if (command.User.IsBot && !global::MFDLabs.Grid.Bot.Properties.Settings.Default.AllowParsingForBots) return;
+
+            if (!global::MFDLabs.Grid.Bot.Properties.Settings.Default.AllowAllChannels)
+            {
+                if (!command.Channel.IsWhitelisted() && !userIsAdmin)
+                    return;
+            }
+
+            if (!global::MFDLabs.Grid.Bot.Properties.Settings.Default.IsEnabled)
+            {
+                if (!userIsAdmin && !userIsPrivilaged)
+                {
+                    SystemLogger.Singleton.Warning("Maintenance enabled, and someone tried to use it!!");
+
+                    var failureMessage = global::MFDLabs.Grid.Bot.Properties.Settings.Default.ReasonForDying;
+
+                    if (!failureMessage.IsNullOrEmpty()) await command.RespondEphemeralPingAsync(failureMessage);
+
+                    return;
+                }
+            }
+
+            if (userIsBlacklisted)
+            {
+                SystemLogger.Singleton.Warning("A blacklisted user {0}('{1}#{2}') tried to use the bot, attempt to DM that they are blacklisted.", command.User.Id, command.User.Username, command.User.Discriminator);
+
+                try
+                {
+                    await command.User.SendDirectMessageAsync($"you are unable to use this bot as you've been blacklisted, to have your case reviewed, please contact <@{(global::MFDLabs.Grid.Bot.Properties.Settings.Default.BotOwnerID)}>");
+                }
+                catch
+                {
+                    SystemLogger.Singleton.Warning("We tried to DM the user, but their DMs may not be available.");
+                }
+
+                return;
+            }
+
+            await CommandRegistry.CheckAndRunSlashCommand(command);
+        }
+    }
+}
+
+#endif
