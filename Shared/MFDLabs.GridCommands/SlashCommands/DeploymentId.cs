4fe6007be5d4da94be4403953849e075b8846b2d
Nikita Petko <petko@vmminfra.net>
2022-03-12 01:29:18 +0000

    GRIDBOT-25 (Deployment and log file commands): Add Commands and Slash Commands

---
+++ Shared/MFDLabs.GridCommands/SlashCommands/DeploymentId.cs
@@ -0,0 +1,40 @@
+﻿#if WE_LOVE_EM_SLASH_COMMANDS
+
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using MFDLabs.Grid.Bot.Extensions;
+using MFDLabs.Grid.Bot.Interfaces;
+using MFDLabs.Grid.Bot.Utility;
+
+namespace MFDLabs.Grid.Bot.SlashCommands
+{
+    internal sealed class DeploymentId : IStateSpecificSlashCommandHandler
+    {
+        public string CommandDescription => "Get Deployment ID";
+        public string CommandAlias => "deployment";
+        public bool Internal => false;
+        public bool IsEnabled { get; set; } = true;
+        public ulong? GuildId => null;
+        public SlashCommandOptionBuilder[] Options => null;
+
+        public async Task Invoke(SocketSlashCommand command)
+        {
+            using (await command.DeferEphemeralAsync())
+            {
+                // The deployment ID is literally just the name of the current directory that the executable is in.
+                // This is not a great way to do this, but it's the best I can think of for now.
+                
+                // Fetch current directory name.
+                var currentDirectory = Directory.GetCurrentDirectory();
+                var currentDirectoryName = Path.GetFileName(currentDirectory);
+
+                // Reply with the deployment ID.
+                await command.RespondEphemeralAsync($"The deployment ID for this instance is: `{currentDirectoryName}`\nPlease paste this into the `Deployment ID` field in grid-bot-support templates to that the internal team can easily identify this instance.");
+            }
+        }
+    }
+}
+
+#endif
---
+++ Shared/MFDLabs.GridCommands/SlashCommands/LogFile.cs
@@ -0,0 +1,32 @@
+﻿#if WE_LOVE_EM_SLASH_COMMANDS
+
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using MFDLabs.Logging;
+using MFDLabs.Grid.Bot.Extensions;
+using MFDLabs.Grid.Bot.Interfaces;
+using MFDLabs.Grid.Bot.Utility;
+
+namespace MFDLabs.Grid.Bot.SlashCommands
+{
+    internal sealed class LogFile : IStateSpecificSlashCommandHandler
+    {
+        public string CommandDescription => "Get Log File Name";
+        public string CommandAlias => "log-file-name";
+        public bool Internal => false;
+        public bool IsEnabled { get; set; } = true;
+        public ulong? GuildId => null;
+        public SlashCommandOptionBuilder[] Options => null;
+
+        public async Task Invoke(SocketSlashCommand command)
+        {
+            using (await command.DeferEphemeralAsync())
+            {
+                await command.RespondEphemeralAsync($"The log file name for this instance is: `{SystemLogger.FileName}`\nPlease paste this into the `Log File Name` field in grid-bot-support templates to that the internal team can easily identify this instance's log files.");
+            }
+        }
+    }
+}
+
+#endif
