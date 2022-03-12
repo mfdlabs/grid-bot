fe69ac8bb036932941cbeebc944f5d01308df480
Nikita Petko <petko@vmminfra.net>
2022-03-12 16:56:41 +0000

    GRIDBOT-25: Deployment, hostname and log file commands (#40)
    * GRIDBOT-25 (Deployment and log file commands): Make FileName public
    
    * GRIDBOT-25 (Deployment and log file commands): Add Commands and Slash Commands
    
    * GRIDBOT-25 (Deployment, hostname and log file commands): Add Commands and Slash Commands

---
+++ Shared/MFDLabs.GridCommands/SlashCommands/Hostname.cs
@@ -0,0 +1,38 @@
+﻿#if WE_LOVE_EM_SLASH_COMMANDS
+
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using MFDLabs.Networking;
+using MFDLabs.Diagnostics;
+using MFDLabs.Grid.Bot.Extensions;
+using MFDLabs.Grid.Bot.Interfaces;
+using MFDLabs.Grid.Bot.Utility;
+
+namespace MFDLabs.Grid.Bot.SlashCommands
+{
+    internal sealed class Hostname : IStateSpecificSlashCommandHandler
+    {
+        public string CommandDescription => "Get Machine Host Name";
+        public string CommandAlias => "hostname";
+        public bool Internal => false;
+        public bool IsEnabled { get; set; } = true;
+        public ulong? GuildId => null;
+        public SlashCommandOptionBuilder[] Options => null;
+
+        public async Task Invoke(SocketSlashCommand command)
+        {
+            using (await command.DeferEphemeralAsync())
+            {
+                await command.RespondEphemeralAsync(
+                    $"The hostname for this instance is: `{SystemGlobal.GetMachineHost()}`\n" +
+                    $"The machine ID for this machine is: `{SystemGlobal.GetMachineId()}`\n" +
+                    $"The IP address for this machine is: `{NetworkingGlobal.GetLocalIp()}`\n" +
+                    "Please paste this into the `Host Name` field in grid-bot-support templates to that the internal team can easily identify this instance."
+                );
+            }
+        }
+    }
+}
+
+#endif
