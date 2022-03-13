d74740beaf71775e79b42f51649e4322fadae971
Nikita Petko <petko@vmminfra.net>
2022-03-13 03:04:21 +0000

    GRIDBOT-27: Counter server IP address restrictions (#43)
    * GRIDBOT-27 (Counter server IP address restrictions): Add new methods to networkng to check ip ranges
    
    * GRIDBOT-27 (Counter server IP address restrictions): Remove unused method
    
    * GRIDBOT-27 (Counter server IP address restrictions): Add methods for replacing only the first occurence of a string
    
    * GRIDBOT-27 (Counter server IP address restrictions): Add IP CIDR check to counterhttpserver, also add a json response type
    
    * GRIDBOT-27 (Counter server IP address restrictions): New list setting for CIDR ranges on the counter server
    
    * GRIDBOT-27 (Counter server IP address restrictions): Implement new setting
    
    * GRIDBOT-27 (Counter server IP address restrictions): Add launch profile for launching bot without gateway

--- Assemblies/MFDLabs.Instrumentation/Http/CounterHttpServer.cs
+++ Assemblies/MFDLabs.Instrumentation/Http/CounterHttpServer.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using MFDLabs.Networking.Extensions;
 
 namespace MFDLabs.Instrumentation
 {
@@ -13,6 +16,15 @@ namespace MFDLabs.Instrumentation
             if (portNumber < _MinPortNumber || portNumber > _MaxPortNumber) 
                 throw new ArgumentOutOfRangeException(nameof(portNumber), string.Format("Invalid value port portNumber: {0}.  Must be between {1} and {2} inclusive.", portNumber, _MinPortNumber, _MaxPortNumber));
             _PortNumber = portNumber;
+            _CounterRegistry = counterRegistry ?? throw new ArgumentNullException(nameof(counterRegistry));
+            _ExceptionHandler = exceptionHandler ?? throw new ArgumentNullException(nameof(exceptionHandler));
+        }
+        public CounterHttpServer(ICounterRegistry counterRegistry, int portNumber, IEnumerable<string> whitelistedCidrRanges, Action<Exception> exceptionHandler)
+        {
+            if (portNumber < _MinPortNumber || portNumber > _MaxPortNumber)
+                throw new ArgumentOutOfRangeException(nameof(portNumber), string.Format("Invalid value port portNumber: {0}.  Must be between {1} and {2} inclusive.", portNumber, _MinPortNumber, _MaxPortNumber));
+            _PortNumber = portNumber;
+            _WhitelistedCidrRanges = whitelistedCidrRanges?.ToArray();
             _CounterRegistry = counterRegistry ?? throw new ArgumentNullException(nameof(counterRegistry));
             _ExceptionHandler = exceptionHandler ?? throw new ArgumentNullException(nameof(exceptionHandler));
         }
@@ -47,9 +59,29 @@ namespace MFDLabs.Instrumentation
         {
             try
             {
+                if (!VerifyAccess(context))
+                {
+                    context.Response.Abort();
+                    return;
+                }
+
                 var counters = (from counter in _CounterRegistry.GetCounterValues()
                                 orderby counter.Key.Category, counter.Key.Name, counter.Key.Instance
                                 select counter).ToArray();
+
+                var values = context.Request.QueryString.GetValues(null);
+
+                if (values != null && values.Contains("json"))
+                {
+                    var result = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new { data = counters.Select(k => new { category = k.Key.Category, name = k.Key.Name, instance = k.Key.Name, value = k.Value }) }));
+
+                    context.Response.StatusCode = 200;
+                    context.Response.ContentType = "application/json";
+                    context.Response.OutputStream.Write(result, 0, result.Length);
+                    context.Response.OutputStream.Close();
+                    return;
+                }
+
                 var responseBuilder = new StringBuilder();
                 responseBuilder.AppendLine("<table border=\"1\" cellpadding=\"3\" cellspacing=\"0\">");
                 responseBuilder.AppendLine("<tr><th>Category</th><th>Name</th><th>Instance</th><th>Value</th></tr>");
@@ -78,9 +110,28 @@ namespace MFDLabs.Instrumentation
             catch (Exception ex) { _ExceptionHandler(ex); }
         }
 
+        private bool VerifyAccess(HttpListenerContext ctx)
+        {
+            if (_WhitelistedCidrRanges == null) return true;
+
+            var remoteIp = ctx.Request.RemoteEndPoint.Address;
+            if (remoteIp == null) return false; 
+
+            var ipTxt = remoteIp.ToString();
+            if (ipTxt == null) return false;
+
+            if (ipTxt == "::1") ipTxt = "127.0.0.1";
+
+            if (ipTxt.IsIpV6())
+                ipTxt = remoteIp.MapToIPv4().ToString();
+
+            return ipTxt.IsInCidrRanges(_WhitelistedCidrRanges);
+        }
+
         private const int _MaxPortNumber = 49151;
         private const int _MinPortNumber = 0;
         private readonly ICounterRegistry _CounterRegistry;
+        private readonly string[] _WhitelistedCidrRanges;
         private readonly int _PortNumber;
         private readonly Action<Exception> _ExceptionHandler;
         private HttpListener _HttpListener;
