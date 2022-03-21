de0f6b1ef5f9ddded4efcaa97fb3e9bba11327d3
Nikita Petko <petko@vmminfra.net>
2022-03-21 05:51:41 +0000

    #46: Add helper methods for auto deployer

--- Assemblies/MFDLabs.FileSystem/FilesHelper.cs
+++ Assemblies/MFDLabs.FileSystem/FilesHelper.cs
@@ -10,7 +10,38 @@ namespace MFDLabs.FileSystem
         private static readonly TimeSpan BaseDelay = TimeSpan.FromSeconds(2.5);
         private static readonly TimeSpan MaxDelay = TimeSpan.FromSeconds(10);
 
-        public static void PollDeletionOfFile(string path, int maxAttempts = 10, Action<Exception> onFailure = null, Action onSuccess = null) 
+        public static bool IsValidPath(string path, bool allowRelativePaths = false)
+        {
+            try
+            {
+                var fullPath = Path.GetFullPath(path);
+
+                if (allowRelativePaths)
+                    return Path.IsPathRooted(path);
+
+                var root = Path.GetPathRoot(path);
+                return !string.IsNullOrEmpty(root.Trim(new char[] { '\\', '/' }));
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool IsSubDir(string parentPath, string childPath)
+        {
+            var parentUri = new Uri(parentPath);
+            var childUri = new DirectoryInfo(childPath).Parent;
+            while (childUri != null)
+            {
+                if (new Uri(childUri.FullName) == parentUri)
+                    return true;
+                childUri = childUri.Parent;
+            }
+            return false;
+        }
+
+        public static void PollDeletionOfFile(string path, int maxAttempts = 10, Action<Exception> onFailure = null, Action onSuccess = null)
             => ThreadPool.QueueUserWorkItem(s => PollDeletionOfFileBlocking(path, maxAttempts, onFailure, onSuccess));
         public static void PollDeletionOfFile(string path, int maxAttempts, Action<Exception> onFailure, Action onSuccess, TimeSpan baseDelay, TimeSpan maxDelay, Jitter jitter = Jitter.None)
             => ThreadPool.QueueUserWorkItem(s => PollDeletionOfFileBlocking(path, maxAttempts, onFailure, onSuccess, baseDelay, maxDelay, jitter));
