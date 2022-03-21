727fae3d10479bc597526c492e7b92763a36ebf9
Nikita Petko <petko@vmminfra.net>
2022-03-21 03:45:57 +0000

    #46: Add .IsMatch and .Match extensions for Regex mathcing

--- Assemblies/MFDLabs.Text/Extensions/StringExtensions.cs
+++ Assemblies/MFDLabs.Text/Extensions/StringExtensions.cs
@@ -21,6 +21,8 @@ namespace MFDLabs.Text.Extensions
         public static string Join<T>(this IEnumerable<T> arr, string seperator) => string.Join(seperator, arr);
         public static string Join<T>(this IEnumerable<T> arr, char seperator) => arr.Join(seperator.ToString());
         public static string ToJson<T>(this T self) => TextGlobal.SerializeJsonWithEnumConverter(self);
+        public static Match Match(this string s, string pattern, RegexOptions options = RegexOptions.None) => Regex.Match(s, pattern, options);
+        public static bool IsMatch(this string s, string pattern, RegexOptions options = RegexOptions.None) => Regex.IsMatch(s, pattern, options);
         public static string MakeFileSafeString(this string s) => Regex.Replace(s, @"[^a-z0-9_-]", "", RegexOptions.IgnoreCase | RegexOptions.Compiled);
         /// <summary>
         /// Thanks @Auxority#5441 for the Regex
