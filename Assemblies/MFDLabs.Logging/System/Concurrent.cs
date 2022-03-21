2d6ef60d738de7a422d276ec18d1f52aae001ca9
Nikita Petko <petko@vmminfra.net>
2022-03-21 05:54:06 +0000

    Move the Lock to LoggingSystem and create concurrent data

--- Assemblies/MFDLabs.Logging/System/Concurrent.cs
+++ Assemblies/MFDLabs.Logging/System/Concurrent.cs
@@ -33,41 +33,4 @@ namespace MFDLabs.Logging.Diagnostics
             return true;
         }
     }
-
-    /// <summary>
-    /// Simple interleave wrapper
-    /// </summary>
-    internal sealed class Interleaver
-    {
-        private readonly Port<Action> _exclusive = new Port<Action>();
-        private readonly Port<Action> _concurrent = new Port<Action>();
-
-        /// <summary>
-        /// DoExclusive
-        /// </summary>
-        /// <param name="action"></param>
-        public void DoExclusive(Action action) => _exclusive.Post(action);
-
-        /// <summary>
-        /// DoConcurrent
-        /// </summary>
-        /// <param name="action"></param>
-        public void DoConcurrent(Action action) => _concurrent.Post(action);
-
-        /// <summary>
-        /// Construct new interleaver
-        /// </summary>
-        public Interleaver(DispatcherQueue q)
-        {
-            Arbiter.Activate(q, Arbiter.Interleave(
-                new TeardownReceiverGroup(),
-                new ExclusiveReceiverGroup(
-                    Arbiter.Receive(true, _exclusive, action => action())
-                ),
-                new ConcurrentReceiverGroup(
-                    Arbiter.Receive(true, _concurrent, action => action())
-                )
-            ));
-        }
-    }
 }
