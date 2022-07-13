9445d22931c3336346887542ab2c5cad2deda241
Nikita Petko <petko@vmminfra.net>
2022-07-13 02:58:57 +0100

    Better Build Scripts (#86)
    * Add more scripts to build without deploying
    
    * Add ability to supply name when publishing release
    
    * Add $releaseNamePrefix arguments to Powershell scripts

---
+++ Scripts/BuildDeployerDebugConfig.ps1
@@ -0,0 +1,1 @@
+.\Build.ps1 -solutionName Services -buildKind MFDLabs.Grid.AutoDeployer -restoreSolution $true -cleanObjAndBinFolders $false -buildConfig "Debug" -buildConcurrently $true -root "..\"
