e5c885b569e2820e61f514131d6200cb6d00b283
Nikita Petko <petko@vmminfra.net>
2022-12-12 02:07:10 +0000

    Implement LOGDEV

--- Assemblies/MFDLabs.Logging/Properties/Settings.Designer.cs
+++ Assemblies/MFDLabs.Logging/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace MFDLabs.Logging.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -92,6 +92,15 @@ namespace MFDLabs.Logging.Properties {
         public bool DefaultLoggerLogWithColor {
             get {
                 return ((bool)(this["DefaultLoggerLogWithColor"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string DefaultLogFileDirectory {
+            get {
+                return ((string)(this["DefaultLogFileDirectory"]));
             }
         }
     }
