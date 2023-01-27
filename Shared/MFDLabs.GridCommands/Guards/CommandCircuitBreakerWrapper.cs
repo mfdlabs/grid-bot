70d2103dab5c84d7e884f65b5a4d52277af863be
Nikita Petko <nkpetko98@gmail.com>
2023-01-27 07:02:49 +0000

    Update CommandCircuitBreakerWrapper.cs

--- Shared/MFDLabs.GridCommands/Guards/CommandCircuitBreakerWrapper.cs
+++ Shared/MFDLabs.GridCommands/Guards/CommandCircuitBreakerWrapper.cs
@@ -15,7 +15,7 @@ namespace MFDLabs.Grid.Bot.Guards
         public CommandCircuitBreakerWrapper(IStateSpecificCommandHandler cmd)
         {
             _command = cmd ?? throw new ArgumentNullException(nameof(cmd));
-            _circuitBreaker = new($"Command '{cmd.CommandName}' Circuit Breaker", ex => ex is not ApplicationException or TimeoutException or EndpointNotFoundException or FaultException, () => RetryInterval);
+            _circuitBreaker = new($"Command '{cmd.CommandName}' Circuit Breaker", ex => ex is not (ApplicationException or TimeoutException or EndpointNotFoundException or FaultException), () => RetryInterval);
         }
         public async Task ExecuteAsync(string[] contentArray, SocketMessage message, string ocn)
             => await _circuitBreaker.ExecuteAsync(async _ => await _command.Invoke(contentArray, message, ocn));
