15edd49f4337d505c08a3939dc223144d84f861a
Nikita Petko <petko@vmminfra.net>
2023-10-07 21:33:14 +0100

    Update Grid.Bot
    Move from services/grid-bot/src to /src
    Move to .NET 6 to accomodate for #150
    Move to native Discord.Net commands.
    Support dependency injection.
    Remove run service scripts.

--- src/Program.cs
+++ src/Program.cs
@@ -20,42 +20,12 @@ public static class Program
         catch (Exception) { return false; }
     }
 
-    private static string GetBadConfigurationError()
-        => $"Could not locate the application configuration at the files '{(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile)}' " +
-        $"or '{(Path.Combine(Directory.GetCurrentDirectory(), "app.config"))}' with your " +
-        "distribution, please install the app correctly and try again.";
-
     /// <summary>
     /// Main method.
     /// </summary>
     /// <param name="args">The arguments.</param>
     public static void Main(string[] args)
     {
-        if (!File.Exists(
-            AppDomain.CurrentDomain.SetupInformation.ConfigurationFile
-        ))
-            if (File.Exists(
-                Path.Combine(
-                    Directory.GetCurrentDirectory(),
-                    "app.config"
-                )
-            ))
-                AppDomain.CurrentDomain.SetupInformation.ConfigurationFile = Path.Combine(
-                    Directory.GetCurrentDirectory(),
-                    "app.config"
-                );
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("[URGENT]: {0}", GetBadConfigurationError());
-                Console.ResetColor();
-
-                if (AssemblyIsLoaded("Backtrace") && AssemblyIsLoaded("Shared.Settings") && AssemblyIsLoaded("Shared.Utility"))
-                    CrashHandlerWrapper.Upload(new ApplicationException(GetBadConfigurationError()));
-
-                return;
-            }
-
         AppDomain.CurrentDomain.UnhandledException += (_, e) =>
         {
             if (e.ExceptionObject is FileNotFoundException or TypeLoadException)
@@ -77,9 +47,7 @@ public static class Program
             Console.ResetColor();
 
             if (AssemblyIsLoaded("Backtrace") && AssemblyIsLoaded("Shared.Settings") && AssemblyIsLoaded("Shared.Utility"))
-                CrashHandlerWrapper.Upload(e.ExceptionObject as Exception);
-
-            Runner.OnGlobalException();
+                Runner.ReportError(e.ExceptionObject as Exception);
 
             if (e.ExceptionObject is AggregateException aggregate)
             {
