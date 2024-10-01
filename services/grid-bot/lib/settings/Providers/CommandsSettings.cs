38783433d8003352cc7687afda67e9d14e24129b
Nikita Petko <petko@vmminfra.net>
2024-10-01 10:07:04 +0100

    Update CommandsSettings.cs
    ~ EnableLockdownCommands: Determines if lockdown commands are enabled, if false then any commands marked with LockdownAttribute will just be passed straight on to the next precondition attribute.
    ~ LockdownGuildId: The guild permitted to respond to lockdown commands.
    ~ TextCommandsDisabledWarningText: Warning text to respond with when text commands are disabled, otherwise it will default to a generic message.
    ~ EnableTextCommands: Determines if text commands should be enabled.
    ~ ShouldWarnWhenCommandsAreDisabled: Determines if a warning should be responded with when the user tries to use a text command.

--- services/grid-bot/lib/settings/Providers/CommandsSettings.cs
+++ services/grid-bot/lib/settings/Providers/CommandsSettings.cs
@@ -11,6 +11,22 @@ public class CommandsSettings : BaseSettingsProvider
     public override string Path => SettingsProvidersDefaults.CommandsPath;
 
     /// <summary>
+    /// Determines whether or not lockdown commands are enabled.
+    /// </summary>
+    public bool EnableLockdownCommands => GetOrDefault(
+        nameof(EnableLockdownCommands),
+        true
+    );
+
+    /// <summary>
+    /// Gets the ID of the guild to use for lockdown commands.
+    /// </summary>
+    public ulong LockdownGuildId => GetOrDefault(
+        nameof(LockdownGuildId),
+        0UL
+    );
+
+    /// <summary>
     /// Gets the command prefix.
     /// </summary>
     public string Prefix => GetOrDefault(
@@ -19,15 +35,41 @@ public class CommandsSettings : BaseSettingsProvider
     );
 
     /// <summary>
-    /// Gets the aliases for all the commands that were in the previous phase (text-based)
+    /// The text to respond with when commands are disabled and the <see cref="ShouldWarnWhenCommandsAreDisabled"/> is true.
+    /// </summary>
+    public string TextCommandsDisabledWarningText => GetOrDefault(
+        nameof(TextCommandsDisabledWarningText),
+        string.Empty
+    );
+
+    /// <summary>
+    /// Determines whether or not text based commands should be enabled.
     /// </summary>
     /// <remarks>
-    /// This is used to make sure that the bot doesn't respond to commands that were not meant for it.
+    /// If this is disabled, the commands module will not be loaded, therefore this cannot
+    /// be used to dynamically load commands.
     /// 
-    /// This setting will be phased out in the future.
+    /// If you wish to consume commands, then this must be enabled before the last shard
+    /// within the connection pool has completed its initialization phase.
+    /// 
+    /// If <see cref="ShouldWarnWhenCommandsAreDisabled"/> is false and this is false, then the bot
+    /// will not respond to any text based commands whatever. Otherwise, it will respond with the text
+    /// defined in <see cref="TextCommandsDisabledWarningText"/> on each subsequent attempt of text
+    /// usage.
     /// </remarks>
-    public string[] PreviousPhaseCommands => GetOrDefault(
-        nameof(PreviousPhaseCommands),
-        Array.Empty<string>()
+    public bool EnableTextCommands => GetOrDefault(
+        nameof(EnableTextCommands),
+        true
+    );
+
+    /// <summary>
+    /// Determines whether or not the bot should warn the user when commands are disabled.
+    /// </summary>
+    /// <remarks>
+    /// This is only applicable when <see cref="EnableTextCommands"/> is false.
+    /// </remarks>
+    public bool ShouldWarnWhenCommandsAreDisabled => GetOrDefault(
+        nameof(ShouldWarnWhenCommandsAreDisabled),
+        true
     );
 }
