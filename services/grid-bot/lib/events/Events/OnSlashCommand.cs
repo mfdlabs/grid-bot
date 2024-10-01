faaaa53db72e6934d5045335152162106f9155af
Nikita Petko <petko@vmminfra.net>
2024-10-01 10:20:11 +0100

    Update OnInteraction.cs
    ~ Drop branch out of interaction service execution to a different thread and just run within the current thread (report back later when it causes problems)

--- services/grid-bot/lib/events/Events/OnSlashCommand.cs
+++ services/grid-bot/lib/events/Events/OnSlashCommand.cs
@@ -183,23 +183,20 @@ public class OnInteraction(
 
         logger.Debug("Executing command '{0}'.", commandName);
 
-        Task.Run(async () =>
-        {
-            using var _ = _interactionProcessingTime
-                .WithLabels(
-                    interaction.Type.ToString()
-                )
-                .NewTimer();
+        using var _ = _interactionProcessingTime
+            .WithLabels(
+                interaction.Type.ToString()
+            )
+            .NewTimer();
 
-            var context = new ShardedInteractionContext(
-                _client,
-                interaction
-            );
+        var context = new ShardedInteractionContext(
+            _client,
+            interaction
+        );
 
-            await _interactionService.ExecuteCommandAsync(
-                context,
-                _services
-            ).ConfigureAwait(false);
-        });
+        await _interactionService.ExecuteCommandAsync(
+            context,
+            _services
+        ).ConfigureAwait(false);
     }
 }
