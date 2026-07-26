d2347dd33cb4d325eddb62d0051e7cc528f21d91
Nikita Petko <petko@vmminfra.net>
2026-07-26 06:36:05 +0100

    #378(@nikita-petko): Update for stage
    #!components: grid-bot
    
    ~ Bump CNPA to v18
    ~ Add pre-run scripts capability
    ~ Update to entrypoint.sh kind

---
+++ services/grid-bot/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/sh
+
+# If PRE_SCRIPTS_DIR defined, then loop thru it and run each script (chmod +x) before starting the main script
+if [ -n "$PRE_SCRIPTS_DIR" ]; then
+  for script in "$PRE_SCRIPTS_DIR"/*.sh; do
+    if [ -f "$script" ]; then
+      echo "Running pre-script: $script"
+
+      dos2unix "$script" 2>/dev/null || true
+      chmod +x "$script"
+
+      # Source the script to run it in the current shell context
+      source "$script"
+    fi
+  done
+fi
+
+dotnet /all/Grid.Bot.dll
